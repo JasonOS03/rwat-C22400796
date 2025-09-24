7eb7e24145c6604b09dba1a7590f890a1c2830e8
JasonOS03 <C22400796@mytudublin.ie>
2025-09-24 20:33:04 +0100

    functional drag and drop

--- lab2/lab2.js
+++ lab2/lab2.js
@@ -1,18 +1,35 @@
 let box = document.getElementById("salmon-box");
+let dragging = false;
+let x = 0;
+let y = 0;
+let newx = 0;
+let newy = 0;
 
-box.addEventListener("dragstart",(e) =>{
-let offx = e.offsetX;
-let offy = e.offsetY;
-e.dataTransfer.setData("text/plain",offx + "," + offy)});
-
-document.body.addEventListener("dragover" , (e) =>
-{
-    e.preventDefault();
+box.addEventListener("mousedown",(e) =>{
+ dragging = true;
+ x = e.clientX;
+ y = e.clientY;
 });
 
-document.body.addEventListener("drop" , (e) =>
+document.body.addEventListener("mousemove" , (e) =>
 {
-    e.preventDefault()
+    e.preventDefault();
+    if(dragging){
+    newx =  e.clientX - x;
+    newy =  e.clientY - y;
+
+    x = e.clientX;
+    y = e.clientY;
+
+    box.style.top = (box.offsetTop + newy) + "px";
+    box.style.left = (box.offsetLeft + newx) + "px";
+    }
+
+});
+
+document.body.addEventListener("mouseup" , (e) =>
+{
+   dragging = false;
 }
-)
+);
 
