fd59f32da6af6dec817ede782ee64f3cb6713df9
JasonOS03 <C22400796@mytudublin.ie>
2025-10-08 14:59:58 +0100

    further JS validation

--- lab4/registration.js
+++ lab4/registration.js
@@ -1,47 +1,44 @@
-let reg_form = document.getElementById("sectionform"); 
-let u_name = document.getElementById("username");
-let p_word = document.getElementById("passw");
-let image = document.getElementById("background");
-let submit_button = document.getElementById("submitform");
+document.addEventListener("DOMContentLoaded", function() {
+    let reg_form = document.getElementById("sectionform"); 
+    let u_name = document.getElementById("username");
+    let p_word = document.getElementById("passw");
+    let image = document.getElementById("background");
+    let submit_button = document.getElementById("submitform");
 
-function validate_uname(u_name)
-{
-    try{
-        if(u_name.length < 8) throw "Username is too short";
-        return true;
-        
+    function validate_uname(u_name) {
+        try {
+            if (!u_name || u_name.length < 8) throw "Username is too short or value has not been entered";
+            return true;
+        } catch(err) {
+            alert(err);
+            return false;
+        }
     }
-    catch(err)
-    {
-        alert(err)
-        return false;
-    }
-}
 
-image.addEventListener("change", function(event) {
-    const file = event.target.files[0]; // 'files', not 'file'
-    if (file) {
-        const tempURL = URL.createObjectURL(file);
-        document.body.style.backgroundImage = `url('${tempURL}')`; // use backticks
+    image.addEventListener("change", function(event) {
+        const file = event.target.files[0]; // 'files', not 'file'
+        if (file) {
+            const tempURL = URL.createObjectURL(file);
+            document.body.style.backgroundImage = `url('${tempURL}')`;
+        }
+    });
+
+    function closeDialog() {
+        const dialog = document.getElementsByTagName("dialog")[0];
+        if(dialog) {
+            dialog.close();
+        }
     }
+
+    function handleSubmit() {
+        let username = u_name.value; // always get current value
+        if(validate_uname(username)) {
+            closeDialog();
+        }
+    }
+
+    submit_button.addEventListener("click", handleSubmit);
 });
 
-function closeDialog()
-{
-     const dialog = document.getElementsByTagName("dialog")[0];
-    if(dialog)
-    {
-        dialog.close();
-    }
-}
-
-function handleSubmit()
-{
-    const uname = document.getElementById("username").value;
-    if(validate_uname(uname))
-    {
-        closeDialog();
-    }
-}
 
 
