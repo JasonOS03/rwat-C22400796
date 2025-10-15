d6e2a6ff53d97138bce233e6b1defba6f6419c92
JasonOS03 <C22400796@mytudublin.ie>
2025-10-15 11:58:57 +0100

    string promise created

--- lab5/promises.js
+++ lab5/promises.js
@@ -1,14 +1,25 @@
-let val = document.getElementById("display_promise");
-
 document.addEventListener("DOMContentLoaded", () => {
-function getNewPromise(value, delay) {
+let val = document.getElementById("display_promise");
+let paragraph_2 = document.getElementById("string_promise");
+let sentence = "This is a new string promise";
+function getNewPromise(value, delay,sentence) {
   return new Promise((resolve, reject) => {
     setTimeout(() => { 
       resolve(value);
     }, delay);
   }).then((res) => {
     val.innerHTML = res;
-  });
+    return sentence;
+  }).then((result) =>{
+    result = result.toUpperCase();
+    return result;
+  }).then((result) =>{
+    result = result.split("").reverse().join("");
+    return result;
+  }).then((result) => {
+    paragraph_2.innerHTML = result;
+  })
+  
 }
-getNewPromise("promise created",2000);
+getNewPromise("promise created",2000,sentence);
 });
