8760353b810db48e5ecde72281cb9136836f91fd
JasonOS03 <C22400796@mytudublin.ie>
2025-11-26 17:12:55 +0000

    Created the firebase app titled RWAT-CA and created the firebase database along with the app. The imports and initialisation code was added to my shapecard.js file, which is my main JavaScript file containing the memory game logic. The firebase BaaS will allow for the persisting of memory game data to a database without the need to create a server in the codebase.

--- CA2/shapecard.js
+++ CA2/shapecard.js
@@ -9,6 +9,25 @@
 
    (see examples below and in the HTML file)
    */
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgiuoHawf7-vdzoLhwumePmeb2djunwSM",
+  authDomain: "rwat-ca-43554.firebaseapp.com",
+  projectId: "rwat-ca-43554",
+  storageBucket: "rwat-ca-43554.firebasestorage.app",
+  messagingSenderId: "1019402551514",
+  appId: "1:1019402551514:web:2811c7ef1a54fb4a7725e3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 
 export class ShapeCard extends HTMLElement {
