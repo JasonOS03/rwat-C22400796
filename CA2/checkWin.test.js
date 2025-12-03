42e8e89f9bc382fd348f6b6f3813068c6d6fbf4b
JasonOS03 <C22400796@mytudublin.ie>
2025-12-03 15:59:50 +0000

    added further test functions. A test has been created in match.test.js to verify what happens when two cards are flipped and either of the attributes do not match. In this case the first card  returns the colour blue and the second card returns the colour red for the colour attribute. In this case the type attributes are expected to equal each other and the colour attributes to not equal each other.The main issues and challenges encountered here were trying to ensure the timer was being called correctly, the useFakeTimer logic has multiple different attributes including AdvanceTimers, but this method is now deprecated and does not work, the UseFakeTimers() needs to be called without any attributes. The next test added is in the CheckWin.test.js to verify what the what happens when the grid size does not match the total cards face up this calls the opposite expect functions to the other checkWin.js test function. The next steps are to add further tests, comment the code and begin implementing the CI/CD pipeline

--- CA2/checkWin.test.js
+++ CA2/checkWin.test.js
@@ -33,3 +33,34 @@ test('verify checkWin is correctly called', async ()=>{
 
 }
 )
+
+test('checkWin is not correctly called when the grid size does not match the total cards face up', async ()=>{
+    const new_div = {
+        querySelectorAll: jest.fn(() =>
+        [
+            {isFaceUp: () => true},
+            {isFaceUp: () => true},
+            {isFaceUp: () => true},
+            {isFaceUp: () => true},
+        ]),
+    };
+    expect(new_div).not.toBeNull();
+
+
+    const db = "fakeDB";
+    const the_size = 6;
+    const num_clicks = 10;
+    global.addDoc = jest.fn();
+    global.collection = jest.fn(() => "fake_collection");
+    global.Timestamp = { now: jest.fn( ()=> "fake_timestamp")};
+    global.alert = jest.fn();
+    console.log = jest.fn();
+
+    await checkWin(db,the_size,new_div,num_clicks);
+    expect(alert).not.toHaveBeenCalledWith("game has been won!");
+    expect(console.log).not.toHaveBeenCalledWith("Database: ",db);
+    expect(addDoc).not.toHaveBeenCalled();
+    expect(collection).not.toHaveBeenCalledWith(db,"memory_game");
+
+}
+)
--- CA2/match.test.js
+++ CA2/match.test.js
@@ -1,5 +1,5 @@
 import { match } from "./match.js";
-import { jest } from '@jest/globals'
+import { jest } from '@jest/globals';
 
 
 test('matching occurs correctly',()=>{
@@ -11,7 +11,52 @@ test('matching occurs correctly',()=>{
 
     expect(res).toBeUndefined();
     expect(card1.getAttribute("colour")).toEqual(card2.getAttribute("colour"));
-    expect(card1.getAttribute("type")).toEqual(card2.getAttribute("type"));
     expect(console.log).toHaveBeenCalledWith("match has been found");
+}
+)
+
+test('matching logic does not occur correctly when card attributes do not match', ()=>{
+    const flip1 = jest.fn();
+    const flip2 = jest.fn();
+    const card1 = 
+    {getAttribute(attribute)
+    {
+        if(attribute == 'colour')
+        {
+            return "blue";
+        }
+        if(attribute == 'type')
+        {
+            return "circle";
+        }
+    }, flip: flip1};
+    const card2 = 
+    {getAttribute(attribute)
+    {
+        if(attribute == 'colour')
+        {
+            return "red";
+        }
+        if(attribute == 'type')
+        {
+            return "circle";
+        }
+    }, flip: flip2};
+
+    jest.useFakeTimers();
+
+    
+
+    console.log = jest.fn();
+    const res = match(card1,card2);
+
+    jest.advanceTimersByTime(1000);
+
+    expect(res).toBeUndefined();
+    expect(card1.getAttribute("colour")).not.toEqual(card2.getAttribute("colour"));
+    expect(card1.getAttribute("type")).toEqual(card2.getAttribute("type"));
+    expect(console.log).not.toHaveBeenCalledWith("match has been found");
+    expect(flip1).toHaveBeenCalled();
+    expect(flip2).toHaveBeenCalled();
 }
 )
